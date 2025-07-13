998e5928622a44ccf5ee6f4dc1daf7b33e8c8713
Raunakk18 <raunakjha654@gmail.com>
2025-07-13 12:23:57 +0530

    Add ToDo-list project

---
+++ ToDo-list/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-slate-500 text-white hover:bg-red-900 rounded-lg p-2'> Hello raunak</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ToDo-list/src/Contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import { useContext , createContext } from "react";
+
+export const TodoContext = createContext({
+    Todos: [
+        {
+            id : 1,
+            Todo : "Todo msg",
+            completed : false
+        }, 
+    ],
+
+    addTodo : ( todo) => {},
+    updataTodo : (id , todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {}
+
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const Todoprovider = TodoContext.Provider();
