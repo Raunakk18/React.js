bdfb98941087b1e6bbc949374db7b3789d160351
Raunakk18 <raunakjha654@gmail.com>
2025-07-19 21:44:20 +0530

    modified UI

--- reduxToolkit/src/App/store.js
+++ reduxToolkit/src/App/store.js
@@ -1,6 +1,6 @@
 import { conigureStore } from '@reduxjs/toolkit';
 import todoReducer from '../Features/Todo/todoslice';
-import reducer from '../Featuress/Todo/todoslice';
+
 
 
 export const store = conigureStore({
