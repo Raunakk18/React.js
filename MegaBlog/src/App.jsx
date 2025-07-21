d2f9f501553d82151ebceda6897585c3e675f66f
Raunakk18 <raunakjha654@gmail.com>
2025-07-21 12:25:30 +0530

    started building Mega Blog

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+    <>
+    <div className="text-white pt-2 bg-slate-500">mega block</div>
+    </>
+  )
+}
+
+export default App
