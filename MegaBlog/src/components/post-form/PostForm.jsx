0421161c62fbbbf0ed884dc478e871940a683c76
Raunakk18 <raunakjha654@gmail.com>
2025-07-27 21:28:53 +0530

    updating post form

--- MegaBlog/src/components/post-form/PostForm.jsx
+++ MegaBlog/src/components/post-form/PostForm.jsx
@@ -5,7 +5,7 @@ import appwriteService from '../../appwrite/config'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-function PostForm() {
+function PostForm({post}) {
     const {register , handleSubmit , watch , setValue, control , getValues} =useForm({
         defaultValues: {
             title: post?.title || '',
@@ -14,6 +14,28 @@ function PostForm() {
             status : post?.status || 'active'
         },
     })
+
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.user.userData)
+
+    const submit = async(data) => {
+      if(post){
+        const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+      }
+      if(file){
+        appwriteService.deleteFile(post.featuredImage)
+      }
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file? file.$id : undefined,
+      })
+
+        if(dbPost){
+          navigate(`/post/${dbPost.$id}`)
+        }
+         
+      }
+
   return (
     <div>PostForm</div>
   )
