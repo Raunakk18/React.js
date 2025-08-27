00a552d660f7a67c1ce12569a0109669ac992e06
Raunakk18 <raunakjha654@gmail.com>
2025-08-27 11:27:40 +0530

    used cors proxy

--- Food-delivery/src/Components/Body.jsx
+++ Food-delivery/src/Components/Body.jsx
@@ -14,6 +14,12 @@ const Body = ()=> {
   }, [])
 
   const fetchData = async() => {
+    // const data = await fetch("https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#")
+
+    // const data = await fetch(
+    //   "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    // );
+
     const data = await fetch(
   "https://api.allorigins.win/raw?url=" + 
   encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
