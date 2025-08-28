11834e8b6013020c65cbed66e1656a3968c8df39
Raunakk18 <raunakjha654@gmail.com>
2025-08-28 11:54:33 +0530

    moved component

--- Food-delivery/src/main.jsx
+++ Food-delivery/src/main.jsx
@@ -2,6 +2,10 @@ import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './Components/Header';
+import Body from './Components/Body';
+import ResturantCard from './Components/ResturantCard';
+import GetImageUrl from './Components/GetImageUrl';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
