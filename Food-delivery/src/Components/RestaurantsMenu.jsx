87cd83c6d95edb6d41364d8bea701ef6c0859523
Raunakk18 <raunakjha654@gmail.com>
2025-08-29 20:59:04 +0530

    call an swiggy API

--- Food-delivery/src/Components/RestaurantsMenu.jsx
+++ Food-delivery/src/Components/RestaurantsMenu.jsx
@@ -3,7 +3,17 @@ import { useEffect } from "react";
 
 const RestaurantsMenu = () => {
 
-    
+    useEffect( () => {
+        fetchMenu()
+    }
+        , [])
+
+    const fetchMenu = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=678579&catalog_qa=undefined&submitAction=ENTER")
+
+        const json = await data.json()
+        console.log(json);
+    }
 
     return (
         <div> my menu</div>
