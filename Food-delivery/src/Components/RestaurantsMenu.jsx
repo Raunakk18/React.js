67b3a936c93a448bed4369d8fce144d5620b9e74
Raunakk18 <raunakjha654@gmail.com>
2025-08-29 21:31:10 +0530

    API fetched successfully

--- Food-delivery/src/Components/RestaurantsMenu.jsx
+++ Food-delivery/src/Components/RestaurantsMenu.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantsMenu = () => {
+
+    const [resInfo , setResInfo] = useState(null)
 
     useEffect( () => {
         fetchMenu()
     }
-        , [])
+        , []);
 
-    const fetchMenu = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=678579&catalog_qa=undefined&submitAction=ENTER")
+const fetchMenu = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=678579&catalog_qa=undefined&submitAction=ENTER")
 
-        const json = await data.json()
-        console.log(json);
-    }
+    const json = await data.json()
+    setResInfo(json.data)
+    console.log(json);
+}
 
-    return (
-        <div> my menu</div>
+
+    return resInfo === null ? 
+    (<Shimmer/> 
+    ) : (
+        <div> 
+            <h1 className="text-red-950">{resInfo?.data?.cards[2]?.card?.card?.info?.areaName}</h1>
+        </div>
     )
 }
 
