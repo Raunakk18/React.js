cc0b3cc2c19029e49452228e9b3e7dcd30e6113d
Raunakk18 <raunakjha654@gmail.com>
2025-08-30 20:13:21 +0530

    API working successfully

--- Food-delivery/src/Components/RestaurantsMenu.jsx
+++ Food-delivery/src/Components/RestaurantsMenu.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const RestaurantsMenu = () => {
@@ -8,25 +7,38 @@ const RestaurantsMenu = () => {
 
     useEffect( () => {
         fetchMenu()
+    } , [])
+
+    const fetchMenu = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=296658&catalog_qa=undefined&submitAction=ENTER")
+
+        const json = await data.json()
+        console.log(json);
+        
+        const resArray = json?.data?.cards
+      ?.map((c) => c.card?.card?.info)
+      .filter(Boolean);
+
+    setResInfo(resArray);
+
     }
-        , []);
 
-const fetchMenu = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.07480&lng=72.88560&restaurantId=296658&catalog_qa=undefined&submitAction=ENTER")
+    
 
-    const json = await data.json()
-    setResInfo(json.data)
-    console.log(json);
-}
-
-
-    return resInfo === null ? 
-    (<Shimmer/> 
+    return resInfo === null ? (
+        <Shimmer/>
     ) : (
-        <div> 
-            <h1></h1>
+        <div>
+            {resInfo.map((info, i) => (
+        <div key={i} className=" p-4" >
+            <h1 className="text-2xl font-bold">{info.name}</h1>
+            <h2 className="text-xl font-medium ">{info.cuisines?.join(", ")}</h2>
+            <h2 className=" text-base font-medium">⭐ {info.avgRatingString}</h2>
+            <h2 className=" text-base font-medium">💰 {info.costForTwoMessage}</h2>
+        </div>
+        ))}
         </div>
     )
 }
 
-export default RestaurantsMenu;
+export default RestaurantsMenu
