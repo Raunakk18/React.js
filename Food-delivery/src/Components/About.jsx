93a13521af13889baff6633c345611f38d35a5c6
Raunakk18 <raunakjha654@gmail.com>
2025-09-05 21:25:58 +0530

    using class based component

--- Food-delivery/src/Components/About.jsx
+++ Food-delivery/src/Components/About.jsx
@@ -4,7 +4,8 @@ const About = () => {
     return(
         <div >
             <h1 className="flex justify-center items-center text-2xl font-medium my-5">
-                Welcome to our Food delivery website</h1>
+                Welcome to our Food delivery website
+            </h1>
         </div>
     )
 }
---
+++ Food-delivery/src/Components/User.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const User = () => {
+    return(
+        <div>
+            <h1>Name : Raunak Jha</h1>
+            <h2> Location : Mumbai</h2>
+            <h3>Contact : 1234567890</h3>
+        </div>
+    )
+}
+
+export default User
